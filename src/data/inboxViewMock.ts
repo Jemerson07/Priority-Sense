450b50772f3ff6193c00bed6fce079a0d671dae9
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-04-16 17:29:23 -0300

    feat: add final inbox view mock dataset

---
+++ src/data/inboxViewMock.ts
@@ -0,0 +1,6 @@
+import { inboxWorkspaceMock } from './inboxWorkspaceMock';
+
+export const inboxViewMock = {
+  tickets: inboxWorkspaceMock.tickets,
+  selectedTicket: inboxWorkspaceMock.selectedTicket,
+};
